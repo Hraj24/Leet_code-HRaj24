0d7cd5ee65c52e3a5f64a2f690e4dbfd004600dc
Harsh Raj <harahjha2002@gmail.com>
2025-08-26 12:37:44 +0530

    Time: 0 ms (100%), Space: 43.5 MB (10.77%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,8 @@
 class Solution {
     public double myPow(double x, int n) {
+
+        //////Bit Manipulation Method
+
         // if(n < 0){
         //     n = -n;
         //     x = 1 / x;
@@ -20,23 +23,51 @@ class Solution {
         // return pow;
 
 
-         if (n == 0)
-        { 
-            return 1;
-        }
-        
-        double half = myPow(x, n / 2);
 
-        if (n % 2 == 0) 
-        {
-            return half * half;
-        }
-        if (n < 0) 
-        {
-            return half * half / x;
+
+        ////// Recursion Method
+
+    //     if (n == 0)
+    //     { 
+    //         return 1;
+    //     }
         
-        }
+    //     double half = myPow(x, n / 2);
+
+    //     if (n % 2 == 0) 
+    //     {
+    //         return half * half;
+    //     }
+    //     if (n < 0) 
+    //     {
+    //         return half * half / x;
         
-        return half * half * x;
+    //     }
+        
+    //     return half * half * x;
+
+
+
+
+    ////// Loop Method
+        double ans = 1.0;
+        long nn = n;
+        if (nn < 0){
+            nn = -1 * nn;
+        }
+        while (nn > 0) {
+            if (nn % 2 == 1) {
+                ans = ans * x;
+                nn = nn - 1;
+            } else {
+                x = x * x;
+                nn = nn / 2;
+            }
+        }
+        if (n < 0){
+            ans = (double)(1.0) / (double)(ans);
+        } 
+        return ans;
+
     }
 }
