7b1b14f3450b69031aa6d905d29c6ca2c508b681
Harsh Raj <harahjha2002@gmail.com>
2025-10-06 11:15:30 +0530

    Time: 7 ms (37.79%), Space: 53.4 MB (22.41%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,43 +3,43 @@ class Solution {
 
 
     // O(n)
-        int n = nums.length;
-        int count = 0; 
-        int element = 0; 
+    //     int n = nums.length;
+    //     int count = 0; 
+    //     int element = 0; 
 
-        for (int i = 0; i < n; i++) {
-            if (count == 0) {
-                element = nums[i];
-            }
-            if (element == nums[i]){
-                count++;
-            } 
-            else{
-                count--;
-            }
+    //     for (int i = 0; i < n; i++) {
+    //         if (count == 0) {
+    //             element = nums[i];
+    //         }
+    //         if (element == nums[i]){
+    //             count++;
+    //         } 
+    //         else{
+    //             count--;
+    //         }
                 
-        }
-        return element;
-    }
+    //     }
+    //     return element;
+    // }
 
 
 //O(n*logn)
-    // Arrays.sort(nums);
-    // int freq=1;
-    // int ans=nums[0];
-    // for(int i=1;i<nums.length;i++){
-    //     if(nums[i]==nums[i-1]){
-    //         freq++;
-    //     }else{
-    //         freq=1;
-    //         ans=nums[i];
-    //     }
+        Arrays.sort(nums);
+        int freq=1;
+        int ans=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
+                freq++;
+            }else{
+                freq=1;
+                ans=nums[i];
+            }
 
 
-    //     if(freq>(nums.length/2)){
-    //         return ans;
-    //     }
-    // }
-    // return ans;
-    // }
+            if(freq>(nums.length/2)){
+                return ans;
+            }
+        }
+        return ans;
+    }
 }
