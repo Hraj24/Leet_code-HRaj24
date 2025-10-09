d22110bb8a6ec6582dba872ebbc7804151151656
Harsh Raj <harahjha2002@gmail.com>
2025-10-09 19:32:45 +0530

    Time: 4 ms (45.18%), Space: 45.6 MB (44.46%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
@@ -1,23 +1,56 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int n = grid.length;
-        int[] map = new int[n * n + 1];
-        int[] ans = new int[2];
+    //     int n = grid.length;
+    //     int[] map = new int[n * n + 1];
+    //     int[] ans = new int[2];
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                map[grid[i][j]]++;
+    //     for(int i = 0; i < n; i++){
+    //         for(int j = 0; j < n; j++){
+    //             map[grid[i][j]]++;
+    //         }
+    //     }
+    //     for(int i = 0; i < map.length; i++) 
+    //     {
+    //         if(map[i] == 2){
+    //             ans[0] = i;  
+    //         }
+    //         if(map[i] == 0){
+    //             ans[1] = i;
+    //         }
+    //     }
+    //     return ans; 
+    // 
+
+
+
+
+        int[] ans = new int[2];
+        Set<Integer> s = new HashSet<>();
+        int n = grid.length;
+        
+        int a = 0; 
+        int b = 0; 
+        
+        int expSum = 0; 
+        int actualSum = 0; 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int currentValue = grid[i][j];
+                actualSum += currentValue;
+                if (s.contains(currentValue)) {
+                    a = currentValue;
+                } else {
+                    s.add(currentValue);
+                }
             }
         }
-        for(int i = 0; i < map.length; i++) 
-        {
-            if(map[i] == 2){
-                ans[0] = i;  
-            }
-            if(map[i] == 0){
-                ans[1] = i;
-            }
-        }
-        return ans; 
+        expSum = (n*n) * ((n*n) + 1) / 2;
+        
+        b = (int) (expSum - actualSum + a);
+
+        ans[0] = a;
+        ans[1] = b; 
+        
+        return ans;
     }
 }
