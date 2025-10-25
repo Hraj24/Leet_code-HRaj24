060f512a706ad3bb3a84b6fe250df3082c71174d
Harsh Raj <harahjha2002@gmail.com>
2025-10-25 11:08:46 +0530

    Time: 28 ms (31.37%), Space: 51.2 MB (6.16%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -21,49 +21,49 @@ class Solution {
 
 //Optimised Approach
         //Using Prefix Sum and HashMap
-    //     int n=nums.length;
-    //     int count=0;
-    //     int prefixSum[]=new int[n];
+        int n=nums.length;
+        int count=0;
+        int prefixSum[]=new int[n];
 
-    //     prefixSum[0]=nums[0];
-    //     for(int i=1;i<n;i++){
-    //         prefixSum[i]=nums[i]+prefixSum[i-1];
-    //     }
-
-    //     Map<Integer,Integer> prefixMap=new HashMap<>();
-    //     for(int i=0;i<n;i++){
-    //         if(prefixSum[i]==k){
-    //             count++;
-    //         }
-    //         int val=prefixSum[i]-k;
-    //         if(prefixMap.contains(val)){
-    //             count+=prefixMap.get(val);
-    //         }
-
-    //         if(!prefixMap.contains(prefixSum[i])){
-    //             prefixMap.set(prefixSum[i])=0;
-    //         }
-    //         prefixMap.set(prefixSum[i])++;
-    //     }
-    //     return count;
-    // }
-
-    //
-        int count = 0;
-        int currentPrefixSum = 0;
-        Map<Integer, Integer> prefixMap = new HashMap<>();
-        prefixMap.put(0, 1); 
-        
-        for (int num : nums) {
-            currentPrefixSum += num;
-            int targetOldSum = currentPrefixSum - k;
-            if (prefixMap.containsKey(targetOldSum)) {
-                count += prefixMap.get(targetOldSum);
-            }
-            prefixMap.put(currentPrefixSum, prefixMap.getOrDefault(currentPrefixSum, 0) + 1);
+        prefixSum[0]=nums[0];
+        for(int i=1;i<n;i++){
+            prefixSum[i]=nums[i]+prefixSum[i-1];
         }
-        
+
+        Map<Integer,Integer> prefixMap=new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(prefixSum[i]==k){
+                count++;
+            }
+            int val=prefixSum[i]-k;
+            if(prefixMap.containsKey(val)){
+                count+=prefixMap.get(val);
+            }
+
+            if(!prefixMap.containsKey(prefixSum[i])){
+                prefixMap.put(prefixSum[i],0);
+            }
+            prefixMap.put(prefixSum[i], prefixMap.get(prefixSum[i]) + 1);
+        }
         return count;
+
+
+//More Optimised Approach
+        // int count = 0;
+        // int currentPrefixSum = 0;
+        // Map<Integer, Integer> prefixMap = new HashMap<>();
+        // prefixMap.put(0, 1); 
+        
+        // for (int num : nums) {
+        //     currentPrefixSum += num;
+        //     int targetOldSum = currentPrefixSum - k;
+        //     if (prefixMap.containsKey(targetOldSum)) {
+        //         count += prefixMap.get(targetOldSum);
+        //     }
+        //     prefixMap.put(currentPrefixSum, prefixMap.getOrDefault(currentPrefixSum, 0) + 1);
+        // }
+        
+        // return count;
     }
     
 }
