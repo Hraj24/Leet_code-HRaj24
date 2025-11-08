38e18f5f1ff6584e86e673e415629b54192e2d8b
Harsh Raj <harahjha2002@gmail.com>
2025-11-08 11:07:43 +0530

    Time: 1 ms (100%), Space: 43.2 MB (5.61%) - LeetHub

--- 2480-find-subarrays-with-equal-sum/2480-find-subarrays-with-equal-sum.java
+++ 2480-find-subarrays-with-equal-sum/2480-find-subarrays-with-equal-sum.java
@@ -1,17 +1,31 @@
 class Solution {
     public boolean findSubarrays(int[] nums) {
-        if(nums.length < 2) return false;
         
-        Map<Integer, Integer> freq = new HashMap<>();
+        // if(nums.length < 2) return false;
         
-        for(int i = 0; i < nums.length - 1; i++){
-            int sum = nums[i] + nums[i + 1];
-            if(freq.getOrDefault(sum, 0) > 0){
-                return true;
-            }
-            freq.put(sum, freq.getOrDefault(sum, 0) + 1);
+        // Map<Integer, Integer> freq = new HashMap<>();
+        
+        // for(int i = 0; i < nums.length - 1; i++){
+        //     int sum = nums[i] + nums[i + 1];
+        //     if(freq.getOrDefault(sum, 0) > 0){
+        //         return true;
+        //     }
+        //     freq.put(sum, freq.getOrDefault(sum, 0) + 1);
+        // }
+        
+        // return false;
+
+
+
+
+
+
+
+        Set<Integer>set=new HashSet<>();
+        for(int i=1;i<nums.length;i++){
+            int sum= nums[i]+nums[i-1];
+            if(!set.add(sum))return true;
         }
-        
         return false;
     }
 }
