f6f8df37522422eb7d50b2ce568b3a24621ae486
Harsh Raj <harahjha2002@gmail.com>
2026-05-04 11:02:50 +0530

    Time: 0 ms (100%), Space: 46.7 MB (79.66%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -13,29 +13,55 @@
  *     }
  * }
  */
-class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> ans=new ArrayList<>();
-        Queue<TreeNode> queue=new LinkedList<>();
 
-        if(root==null){
-            return ans;
+
+
+// class Solution {
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         List<List<Integer>> ans=new ArrayList<>();
+//         Queue<TreeNode> queue=new LinkedList<>();
+
+//         if(root==null){
+//             return ans;
+//         }
+//         queue.add(root);
+//         while(!queue.isEmpty()){
+//             int level=queue.size();
+//             List<Integer> subLevel=new ArrayList<>();
+//             for(int i=0;i<level;i++){
+//                 if(queue.peek().left!=null){
+//                     queue.add(queue.peek().left);
+//                 }
+//                 if(queue.peek().right!=null){
+//                     queue.add(queue.peek().right);
+//                 }
+//                 subLevel.add(queue.remove().val);
+//             }
+//             ans.add(subLevel);
+//         }
+//         return ans;
+//     }
+// }
+
+
+
+class Solution {
+    public void Order(TreeNode root, List<List<Integer>> ans, int level) {
+        if (root == null) return;
+
+        if (ans.size() == level) {
+            ans.add(new ArrayList<>());
         }
-        queue.add(root);
-        while(!queue.isEmpty()){
-            int level=queue.size();
-            List<Integer> subLevel=new ArrayList<>();
-            for(int i=0;i<level;i++){
-                if(queue.peek().left!=null){
-                    queue.add(queue.peek().left);
-                }
-                if(queue.peek().right!=null){
-                    queue.add(queue.peek().right);
-                }
-                subLevel.add(queue.remove().val);
-            }
-            ans.add(subLevel);
-        }
+
+        ans.get(level).add(root.val);
+
+        Order(root.left, ans, level + 1);
+        Order(root.right, ans, level + 1);
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Order(root, ans, 0);
         return ans;
     }
 }
